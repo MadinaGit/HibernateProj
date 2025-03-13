f2dee79c3cf18a9f57a27bac617768bfbc273c6b
MalachevaM <malachevam@gmail.com>
2025-03-13 18:16:58 +0300

    add new branch

--- src/main/java/ru/netology/hibernateproj/PersonsController.java
+++ src/main/java/ru/netology/hibernateproj/PersonsController.java
@@ -8,23 +8,43 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-    @RequestMapping
-    public class PersonsController {
+@RequestMapping("/persons")
+public class PersonsController {
 
-        private final PersonsRepository personsRepository;
+    private final PersonsRepository personsRepository;
 
-        @Autowired
-        public PersonsController(PersonsRepository personsRepository) {
-            this.personsRepository = personsRepository;
-        }
+    @Autowired
+    public PersonsController(PersonsRepository personsRepository) {
+        this.personsRepository = personsRepository;
+    }
 
-    @GetMapping("/persons/by-city")
+    @GetMapping("/by-city")
     public ResponseEntity<List<Persons>> getPersonsByCity(@RequestParam String city) {
-        List<Persons> persons = personsRepository.getPersonsByCityOfLiving(city);
+        List<Persons> persons = personsRepository.findByCityOfLiving(city);
+        if (persons.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
         return ResponseEntity.ok(persons);
     }
+
+    @GetMapping("/by-age")
+    public ResponseEntity<List<Persons>> getPersonsAgeLessThan(@RequestParam int age) {
+        List<Persons> persons = personsRepository.findByAgeLessThanOrderByAgeAsc(age);
+        if (persons.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(persons);
     }
+
+    @GetMapping("/by-name-and-surname")
+    public ResponseEntity<Persons> getPersonsByNameAndSurname(@RequestParam String name, @RequestParam String surname) {
+        Optional<Persons> persons = personsRepository.findByNameAndSurname(name, surname);
+        return persons.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
 
 
--- src/main/java/ru/netology/hibernateproj/PersonsRepository.java
+++ src/main/java/ru/netology/hibernateproj/PersonsRepository.java
@@ -4,11 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @Transactional
 public interface PersonsRepository extends JpaRepository<Persons, Long> {
-    List<Persons> getPersonsByCityOfLiving(String cityOfLiving);
+    List<Persons> findByCityOfLiving(String cityOfLiving);
+
+    List<Persons> findByAgeLessThanOrderByAgeAsc(int age);
+
+    Optional<Persons> findByNameAndSurname(String name, String surname);
+
 }
